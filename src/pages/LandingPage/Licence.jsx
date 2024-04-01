29e1ddff4734c18f42119f80982e274b5a579947
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2024-04-01 23:22:38 +0100

    chore:Change header image

--- src/pages/LandingPage/Licence.jsx
+++ src/pages/LandingPage/Licence.jsx
@@ -4,34 +4,32 @@ import NccIcon from "../../assets/icons/ncc.svg";
 import NitdaIcon from "../../assets/icons/nitda.svg";
 import PicIcon from "../../assets/icons/pic.svg";
 import PssbIcon from "../../assets/icons/pssb.svg";
-import SanefIcon from "../../assets/icons/sanef.svg";
-import InterswitchIcon from "../../assets/icons/interswitch.svg";
+// import SanefIcon from "../../assets/icons/sanef.svg";
+// import InterswitchIcon from "../../assets/icons/interswitch.svg";
 // import GrandtowersIcon from "../../assets/icons/grandtowers.svg";
 export const LicenceSection = () => {
   return (
-    <div className="md:flex my-8 bg-[#e5effd] gap-5 hidden md:visible">
-      <div className="font-normal text-lg w-40"> Trusted by: </div>
-      <div>
-        <img src={CbnIcon} alt="icon" />
+    <div className="md:flex items-center my-8 bg-[#e5effd] hidden">
+      <div className="font-normal text-lg w-48">
+        <p>Trusted by:</p>
       </div>
-      <div className="mt-2">
-        <img src={NccIcon} alt="icon" />
+      <div className="flex space-x-5">
+        <div>
+          <img src={CbnIcon} alt="icon" />
+        </div>
+        <div className="mt-2">
+          <img src={NccIcon} alt="icon" />
+        </div>
+        <div>
+          <img src={NitdaIcon} alt="icon" />
+        </div>
+        <div className="mt-2">
+          <img src={PicIcon} alt="icon" />
+        </div>
+        <div className="mt-2">
+          <img src={PssbIcon} alt="icon" />
+        </div>
       </div>
-      <div>
-        <img src={NitdaIcon} alt="icon" />
-      </div>
-      <div className="mt-2">
-        <img src={PicIcon} alt="icon" />
-      </div>
-      <div className="mt-2">
-        <img src={PssbIcon} alt="icon" />
-      </div>
-      {/* <div className="lg:p-7 ">
-        <img src={SanefIcon} alt="icon" />
-      </div>
-      <div className="lg:p-6 ">
-        <img src={InterswitchIcon} alt="icon" />
-      </div> */}
     </div>
   );
 };
