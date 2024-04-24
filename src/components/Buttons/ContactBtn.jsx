1edfd054db061ae8000e6b56f5f9f9f46abc252d
Isa-Muhammad <isamuhammadm.im@gmail.com>
2024-04-24 11:09:39 +0100

    chore: Add customer support whatsapp contact

--- src/components/Buttons/ContactBtn.jsx
+++ src/components/Buttons/ContactBtn.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { WhatsAppOutlined } from "@ant-design/icons";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Contactbtn = () => {
   return (
     <div>
       <Link to="/contact">
-      <button className="rounded-md bg-indigo-600 hover:bg-indigo-700 text-white min-w-[200px] py-3 -translate-y-2 px-14 font-medium">
-        Contact
-      </button>
+        <button className="rounded-md bg-indigo-600 hover:bg-indigo-700 text-white min-w-[200px] py-3 -translate-y-2 px-14 font-medium">
+          Contact
+        </button>
       </Link>
     </div>
   );
@@ -16,10 +16,10 @@ export const Contactbtn = () => {
 export const ContactbtOutline = () => {
   return (
     <div>
-     <Link to="/contact">
-      <button className="rounded-md bg-indigo-500 hover:bg-indigo-600 text-white text-lg px-16 outline border-white py-3">
-        Contact
-      </button>
+      <Link to="/contact">
+        <button className="rounded-md bg-indigo-500 hover:bg-indigo-600 text-white text-lg px-16 outline border-white py-3">
+          Contact
+        </button>
       </Link>
     </div>
   );
@@ -27,13 +27,13 @@ export const ContactbtOutline = () => {
 export const WhatsappOutline = () => {
   return (
     <div>
-     <a href="tel:+23456789100000">
-      <button
-        className="rounded-md  hover:bg-indigo-700 text-white text-lg px-20 outline border-white py-3"
-        icon={<WhatsAppOutlined />}
-      >
-        <WhatsAppOutlined />
-      </button>
+      <a href="https://wa.me/+2349061163130">
+        <button
+          className="rounded-md  hover:bg-indigo-700 text-white text-lg px-20 outline border-white py-3"
+          icon={<WhatsAppOutlined />}
+        >
+          <WhatsAppOutlined />
+        </button>
       </a>
     </div>
   );
